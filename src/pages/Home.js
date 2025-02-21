dc024f48eb3d6d3ae5c1016c4791e172d5e48998
marcos <martinmarcospablo@gmail.com>
2025-02-21 20:05:44 -0300

    carousel

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import Carousel from './Carousel';
+
 
 const Home = () => {
   return (
     <div className="home-container">
       <img src="/portada.png" alt='Logo' className="home-image" />
+      <Carousel />
       <h1>Bienvenido a nuestra Tienda</h1>
       <p>Compra lo que necesitas de manera rápida y sencilla.</p>
       <Link to="/login">
