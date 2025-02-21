b0c8c608fd93c7ed765c5aaadba26792888109f6
marcos <martinmarcospablo@gmail.com>
2025-02-21 20:08:49 -0300

    nuevas ubicaciones

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -7,9 +7,11 @@ const Home = () => {
   return (
     <div className="home-container">
       <img src="/portada.png" alt='Logo' className="home-image" />
-      <Carousel />
+      
       <h1>Bienvenido a nuestra Tienda</h1>
       <p>Compra lo que necesitas de manera rápida y sencilla.</p>
+      <Carousel />
+      <h2>¿Ya tienes una cuenta?</h2>
       <Link to="/login">
         <button>Ingresar</button>
       </Link>
